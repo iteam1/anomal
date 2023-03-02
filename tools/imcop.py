507b39ea10ec22dc52de1c8ebd3241ba9b128500
locchuong <locchuong9@gmail.com>
2023-03-02 16:40:51 +0700

    update

--- tools/imcop.py
+++ tools/imcop.py
@@ -25,7 +25,7 @@ for thing in things:
     if a and b and c:
         dates.append(thing)
 
-n = [1,2,3,4,5,6]
+n = [1,2,3]
 
 for date in dates:
     print(date)
@@ -38,15 +38,11 @@ for date in dates:
         d = random.choice(n) == 1 # random sampling
         # if all condition sartified
         if a and b and c and d:
-            file1 = os.path.join(date,item,'pictures/top.jpg')
-            file2 = os.path.join(date,item,'image_crop/top_crop.jpg')
-            a = os.path.exists(file1)
-            b = os.path.exists(file2)
+            file = os.path.join(date,item,'image_crop/top_crop.jpg')
+            b = os.path.exists(file)
             if a and b:
-                name = str(item) + "_top.jpg"
-                shutil.copy(file1,os.path.join(dst,name))
                 name = str(item) + "_top_crop.jpg"
-                shutil.copy(file2,os.path.join(dst,name))
+                shutil.copy(file,os.path.join(dst,name))
                 print('coping:',item)
                 
 # compress files
