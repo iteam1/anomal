9802d3ba156cf9062b189920aa72e7b215ebc71f
locchuong <locchuong9@gmail.com>
2023-03-03 15:54:01 +0700

    rename config files

--- utils/crop.py
+++ utils/crop.py
@@ -12,11 +12,10 @@ src = sys.argv[1]
 tmp = sys.argv[2]
 
 dst = 'datasets'
-cat1 = 'lap0'
-cat2 = 'lap1'
+cat = 'laptop'
 
 DIM = 512
-n = [1,2]
+n = [1,2,3]
 count = 0
 
 def rename(count):
@@ -37,53 +36,31 @@ if not os.path.exists(src):
 if not os.path.exists(dst):
     os.mkdir(dst)
 
-path = os.path.join(dst,cat1)
+path = os.path.join(dst,cat)
 if not os.path.exists(path):
     os.mkdir(path)
     
-path = os.path.join(dst,cat1,tmp)
-if not os.path.exists(path):
-    os.mkdir(path)
-
-path = os.path.join(dst,cat2)
-if not os.path.exists(path):
-    os.mkdir(path)
-    
-path = os.path.join(dst,cat2,tmp)
+path = os.path.join(dst,cat,tmp)
 if not os.path.exists(path):
     os.mkdir(path)
     
 imgs  = os.listdir(src)
 
-# gray
 for i in imgs:
     v = random.choice(n)
-    if v == 1:
+    if v == 1 or v == 2:
         path = os.path.join(src,i)
-        img = cv2.imread(path,0)
-        img1 = cv2.imread(path)
-        h,w = img.shape
-        
+        img = cv2.imread(path)
+        h,w,_ = img.shape
         top_left = img[0:DIM,0:DIM]
-        top_left1 = img1[0:DIM,0:DIM]
-        
         name = rename(count)
-        path = os.path.join(dst,cat1,tmp,name)
-        cv2.imwrite(path,top_left)
-        path = os.path.join(dst,cat2,tmp,name)
-        cv2.imwrite(path,top_left1)
-        
+        path = os.path.join(dst,cat,tmp,name)
+        cv2.imwrite(path,top_left)     
         count +=1
-        
         top_right = img[0:DIM,w-DIM:w]
-        top_right1 = img1[0:DIM,w-DIM:w] 
-        
         name = rename(count)
-        path = os.path.join(dst,cat1,tmp,name)
+        path = os.path.join(dst,cat,tmp,name)
         cv2.imwrite(path,top_right)
-        path = os.path.join(dst,cat2,tmp,name)
-        cv2.imwrite(path,top_right1)
-        
         count +=1
 
 print("Total:",count)
