01b3940c57a1aacb663588c1f31a6fb30808eb09
locchuong <locchuong9@gmail.com>
2023-03-06 16:37:42 +0700

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ virtualenv env
 source env/bin/activate
 
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+pip install openvino-dev==2021.3.0
+pip install anomalib==0.4.0
 pip install -r requirements/base.txt
-pip install -r requirements/openvino.txt
-#pip install anomalib==0.4.0
