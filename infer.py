006f9f2f9412442c07ae8eb272217ccd6557c180
locchuong <locchuong9@gmail.com>
2023-03-06 16:15:25 +0700

    update

--- infer.py
+++ infer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import cv2
 import time
 import numpy as np
@@ -7,7 +8,7 @@ from anomalib.deploy import TorchInferencer
 #from anomalib.deploy.inferencers.torch_inferencer import TorchInferencer
 #from anomalib.deploy.inferencers.openvino_inferencer import OpenVINOInferencer
 
-visualizer = Visualizer(mode="full",task="segmentation")
+visualizer = Visualizer(mode="simple",task="segmentation")
 
 # config_path = 'models/patchcore/mvtec/hazelnut/run/config.yaml'
 # weight_path = 'models/patchcore/mvtec/hazelnut/run/openvino/model.xml' # yml,onnx
@@ -21,14 +22,14 @@ visualizer = Visualizer(mode="full",task="segmentation")
 #     device = device
 # )
 
-config_path = 'models/fastflow/mvtec/laptop/run/config.yaml'
-model_path = 'models/fastflow/mvtec/laptop/run/weights/model.ckpt'
+config_path = 'models//mvtec/laptop/run/config.yaml'
+model_path = 'models/cfa2/mvtec/laptop/run/weights/model.ckpt'
 inferencer = TorchInferencer(config=config_path,model_source=model_path,device ='auto')
 
 if __name__ == "__main__":
     # Start counting time
     start_time = time.time()
-    image = cv2.imread('datasets/laptop/test/crack/002.png')
+    image = cv2.imread('datasets/laptop/test/crack/005.png')
     # predict
     prediction = inferencer.predict(image=image)
     output = visualizer.visualize_image(prediction)
