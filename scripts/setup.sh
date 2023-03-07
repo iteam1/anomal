501b69fbb9464370071c0a04523de02e9ab7aaa7
locchuong <locchuong9@gmail.com>
2023-03-07 13:13:13 +0700

    update init

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,5 @@ source env/bin/activate
 
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
 pip install anomalib==0.4.0
-#pip install openvino-dev==2021.3.0
+pip install -r requirements/openvino.txt
 #pip install -r requirements/base.txt
-#pip install -r requirements/openvino.txt
--- utils/infer.py
+++ utils/infer.py
@@ -4,34 +4,32 @@ import cv2
 import time
 import random
 import numpy as np
+import argparse
 from anomalib.post_processing import Visualizer
 from anomalib.deploy import TorchInferencer, OpenVINOInferencer
 #from anomalib.post_processing.visualizer import Visualizer
 #from anomalib.deploy.inferencers.torch_inferencer import TorchInferencer
 #from anomalib.deploy.inferencers.openvino_inferencer import OpenVINOInferencer
 
+# initialize
 model_name = sys.argv[1]
 path = 'samples/crack'
 DIM = 256
 
 # visualizer
 visualizer = Visualizer(mode="simple",task="segmentation")
-
-# config_path = 'model/dfm/mvtec/laptop/run/config.yaml'
-# weight_path = 'model/dfm/mvtec/laptop/run/openvino/model.onnx'
-# meta_data_path = 'models/dfm/mvtec/laptop/run/openvino/meta_data.json'
-# device = 'CPU' #["CPU", "GPU", "VPU"]
-# inferencer = OpenVINOInferencer(
-#     config = config_path,
-#     path = weight_path,
-#     meta_data_path = meta_data_path,
-#     device = device
-# )
+# directory of ouput image
+image_path = f'results/{model_name}/mvtec/laptop/run/images'
 
 config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
-model_path = f'model/{model_name}/mvtec/laptop/run/weights/model.ckpt'
-inferencer = TorchInferencer(config=config_path,model_source=model_path,device ='auto')
-image_path = f'results/{model_name}/mvtec/laptop/run/images'
+weight_path = f'model/{model_name}/mvtec/laptop/run/openvino/model.onnx'
+meta_data_path = f'model/{model_name}/mvtec/laptop/run/openvino/meta_data.json'
+device = 'CPU' #["CPU", "GPU", "VPU"]
+inferencer = OpenVINOInferencer(config = config_path,path = weight_path,meta_data_path = meta_data_path,device = device)
+
+# config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
+# model_path = f'model/{model_name}/mvtec/laptop/run/weights/model.ckpt'
+# inferencer = TorchInferencer(config=config_path,model_source=model_path,device ='auto')
 
 images = os.listdir(path)
 image = random.choice(images)
@@ -46,17 +44,17 @@ if __name__ == "__main__":
 
     # predict top left
     prediction = inferencer.predict(image=top_left)
-    output = visualizer.visualize_image(prediction)
-    output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
-    path = os.path.join(image_path,'top_left_infer.jpg')
-    cv2.imwrite(path,output)
+    # output = visualizer.visualize_image(prediction)
+    # output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
+    # path = os.path.join(image_path,'top_left_infer.jpg')
+    # cv2.imwrite(path,output)
 
     # predict top left
     prediction = inferencer.predict(image=top_right)
-    output = visualizer.visualize_image(prediction)
-    output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
-    path = os.path.join(image_path,'top_right_infer.jpg')
-    cv2.imwrite(path,output)
+    # output = visualizer.visualize_image(prediction)
+    # output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
+    # path = os.path.join(image_path,'top_right_infer.jpg')
+    # cv2.imwrite(path,output)
 
     end_time = time.time() - start_time
     print("Inference timing consumption:",end_time)
