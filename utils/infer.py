bc55614cbb0e5b908b0b4cacf761ed10b2be9481
locchuong <locchuong9@gmail.com>
2023-03-07 13:35:45 +0700

    update

--- utils/infer.py
+++ utils/infer.py
@@ -4,32 +4,45 @@ import cv2
 import time
 import random
 import numpy as np
-import argparse
+from argparse import ArgumentParser,Namespace
 from anomalib.post_processing import Visualizer
 from anomalib.deploy import TorchInferencer, OpenVINOInferencer
-#from anomalib.post_processing.visualizer import Visualizer
-#from anomalib.deploy.inferencers.torch_inferencer import TorchInferencer
-#from anomalib.deploy.inferencers.openvino_inferencer import OpenVINOInferencer
+
+def get_args() -> Namespace:
+    '''
+    Get command line arguments
+    '''
+    parser = ArgumentParser()
+    parser.add_argument('--model',type=str,default = 'padim',
+                        choices=["cfa", "cflow", "dfkde","dfm","padim",'patchcore','reverse_distillation','stfpm'],
+                        help = 'Name of the trained model')
+    parser.add_argument('--openvino',action='store_true',help='Option optmize by openvino')
+    parser.add_argument('--dim',type=int,default=256,help='Image crop size')
+    parser.add_argument('--path',type=str,default='samples/crack',help='Path of Predict Image')
+    args = parser.parse_args()
+    return args
 
 # initialize
-model_name = sys.argv[1]
-path = 'samples/crack'
-DIM = 256
+args = get_args()
+model_name = args.model
+path = args.path
+DIM = args.dim
 
 # visualizer
 visualizer = Visualizer(mode="simple",task="segmentation")
 # directory of ouput image
 image_path = f'results/{model_name}/mvtec/laptop/run/images'
 
-config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
-weight_path = f'model/{model_name}/mvtec/laptop/run/openvino/model.onnx'
-meta_data_path = f'model/{model_name}/mvtec/laptop/run/openvino/meta_data.json'
-device = 'CPU' #["CPU", "GPU", "VPU"]
-inferencer = OpenVINOInferencer(config = config_path,path = weight_path,meta_data_path = meta_data_path,device = device)
-
-# config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
-# model_path = f'model/{model_name}/mvtec/laptop/run/weights/model.ckpt'
-# inferencer = TorchInferencer(config=config_path,model_source=model_path,device ='auto')
+if args.openvino:
+    config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
+    weight_path = f'model/{model_name}/mvtec/laptop/run/openvino/model.onnx'
+    meta_data_path = f'model/{model_name}/mvtec/laptop/run/openvino/meta_data.json'
+    device = 'CPU' #["CPU", "GPU", "VPU"]
+    inferencer = OpenVINOInferencer(config = config_path,path = weight_path,meta_data_path = meta_data_path,device = device)
+else:
+    config_path = f'model/{model_name}/mvtec/laptop/run/config.yaml'
+    model_path = f'model/{model_name}/mvtec/laptop/run/weights/model.ckpt'
+    inferencer = TorchInferencer(config=config_path,model_source=model_path,device ='auto')
 
 images = os.listdir(path)
 image = random.choice(images)
