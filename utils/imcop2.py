74822bbd31f573cb99dfa366ad42e972b8f078f4
locchuong <locchuong9@gmail.com>
2023-03-09 09:45:31 +0700

    update tool copy

--- utils/imcop2.py
+++ utils/imcop2.py
@@ -35,29 +35,29 @@ for date in dates:
         a = os.path.isdir(path)
         b = len(item) == 14
         c = item.isalnum()
-        d = random.choice(n) == 1 # random sampling
+        d =  random.choice(n) == 1 # random sampling
         # if all the conditions are sartified
         if a and b and c and d:
             # original top
-            file1 = os.path.join(date,item,'image_crop/top.jpg')
+            file1 = os.path.join(date,item,'pictures/top.jpg')
             e1 = os.path.exists(file1)
             # top mask
-            file2 = os.path.join(date,item,'image_crop/top_crop.jpg')
+            file2 = os.path.join(date,item,'image_crop/top/image_mask_crop_final.jpg')
             e2 = os.path.exists(file2)
             # top crop
-            file3 = os.path.join(date,item,'image_crop/top/image_mask_crop_final.jpg')
+            file3 = os.path.join(date,item,'image_crop/top_crop.jpg')
             e3 = os.path.exists(file3)
             if e1 and e2 and e3:
+                print('coping:',item)
                 # copy original top
                 name = str(item) + "_top.jpg"
                 shutil.copy(file1,os.path.join(dst,name))
                 # copy mask
-                name = str(item) + "_top_crop.jpg"
+                name = str(item) + "_mask.jpg"
                 shutil.copy(file2,os.path.join(dst,name))
                 # copy top crop
-                name = str(item) + "_top_crop.jpg"
+                name = str(item) + "_crop.jpg"
                 shutil.copy(file3,os.path.join(dst,name))
-                print('coping:',item)      
 # compress files
 cmd = 'tar -zcvf '+dst+'.tar.gz'+' '+dst
 print("Executing ",cmd)
