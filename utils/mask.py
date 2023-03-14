f886663cc814c0a1b4de81088519cf55c79ce289
locchuong <locchuong9@gmail.com>
2023-03-14 15:29:38 +0700

    update dsi

--- utils/mask.py
+++ utils/mask.py
@@ -5,20 +5,19 @@ import shutil
 import numpy as np
 from keras.models import load_model
 from skimage.morphology import closing
-from skimage.morphology import disk  # noqa
+from skimage.morphology import disk
 
 # init
-footprint = disk(6)
 kernel = np.ones((2, 2), np.uint8)
 s = 3 # window side
 p = 50 # patient value
 K = 124
 R = 40 # 40
 DIM = 256
-side = "right"
+side = "e"
 #src = 'test/crack'
-#src = 'test/noise'
-src = 'test/good'
+src = 'test/noise'
+#src = 'test/good'
 src = os.path.join(src,side)
 dst = 'output/mask'
 
@@ -37,33 +36,34 @@ else:
     shutil.rmtree(dst, ignore_errors=True)
     os.mkdir(dst)
 
-def round_corner(img,side,r):
-    # flip image if right side
-    if side == 'right':
-        img = cv2.flip(img,1)
-    h,w,c = img.shape
-    blank = np.zeros((h,w),np.uint8)
-    # r = int(h/2)
-    center = (r,r)
-    axes_length = (r,r)
-    angle  = 180
-    start_angle = 0
-    end_angle = 90
-    color = (255)
-    thickness = -1
-    out = cv2.ellipse(blank,center,axes_length,angle,start_angle,end_angle,color,thickness)
-    out[r:,:] = 255
-    out[:,r:] = 255
-    # revert flip
-    if side == 'right':
-        out = cv2.flip(out,1)
-    return out
+def filter(img,interation):
+    s = 2
+    h,w = img.shape
+    for k in range(interation):
+        for i in range(0,h,s):
+            for j in range(0,w,s):
+                window = img[i:i+s,j:j+s]
+                t = np.sum(window)/255
+                if t < s*s:
+                    img[i:i+s,j:j+s] = 0
+    return img
+
+def outer_filter(img,interation):
+    h,w = img.shape
+    for k in range(interation):
+        for i in range(0,h):
+            for j in range(0,w):
+                window = img[i-1:i+1,j-1:j+1]
+                t = np.sum(window)/255
+                if t == 1:
+                    img[i,j] = 0
+    return img
     
 def mask(img,side,model_line):
     # Convert to gray
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     h,w = gray.shape
-    
+
     # 
     se = cv2.getStructuringElement(cv2.MORPH_RECT , (4,4))
     bg = cv2.morphologyEx(gray, cv2.MORPH_DILATE, se)
@@ -78,9 +78,9 @@ def mask(img,side,model_line):
     pred = pred.reshape(pred.shape[1],pred.shape[2])
     pred = pred * 255
     pred = pred.astype('uint8')
+    
     #closing
-    pred = closing(pred, footprint)
-    pred = cv2.bitwise_and(pred,out_binary)
+    pred = closing(pred, disk(3))
     
     # find vertical line
     v_ = []
@@ -90,13 +90,9 @@ def mask(img,side,model_line):
     vx = np.argmax(v_)
     if side == "left":
         if vx > p: vx = 0
-        pred[:,vx:vx+1] = 255 # draw line
-        pred[:,:vx] = 0 # remove left over part for extracting horizontal line
     else:
         if vx < w - p:
             vx = w
-        pred[:,vx:vx+1] = 255 # draw line
-        pred[:,vx:] = 0 # remove left over part for extracting horizontal line
     
     # find horizontal line
     h_ = []
@@ -107,12 +103,27 @@ def mask(img,side,model_line):
     if hy > p:
         hy = 0
     
-    pred[hy:hy+1,:] = 255
-    pred[:hy,:] = 0 # remove right over part
+    # remove outline
+    pred[:hy,:] = 0 
+    if side == "left":
+        pred[:,:vx] = 0
+    else:
+        pred[:,vx:] = 0
     
-    out = pred        
-    print("vx:",vx,"hy:",hy)
+    # grabcut
+    mask = pred.copy()
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    if side == 'left':
+        rect = (hy,vx,DIM-hy,DIM-vx)
+    else:
+        rect = (hy,0,DIM-hy,vx) 
+    new_mask,fg,bg = cv2.grabCut(img,mask,rect,bgdModel,fgdModel,3,cv2.GC_INIT_WITH_RECT)
+    mask2 = np.where((new_mask==2)|(new_mask==0),0,1).astype('uint8') 
+    img = img*mask2[:,:,np.newaxis]
     
+    out = cv2.bitwise_or(pred,mask2*255)
+
     return out
 
 for i,image in enumerate(images):
